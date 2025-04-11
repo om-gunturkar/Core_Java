d6dd7fe8df76226f8ca28c2c5b4a0383e1145284
Om Gunturkar <omgunturkar@gmail.com>
2025-04-11 16:42:31 +0530

    updated

---
+++ src/Functions/Scope.java
@@ -0,0 +1,14 @@
+package Functions;
+// where we can access our variables
+public class Scope {
+
+    public static void main(String[] args) {
+        // here we can access a only in this function only
+        int a=10;
+        int b=20;
+    }
+
+    static void random(){
+        System.out.println(a); //ERROR : we dont know a here
+    }
+}
